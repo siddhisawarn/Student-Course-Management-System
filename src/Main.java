0ca46fc7ab7e785765018a35364c0018ad5c4b76
siddhisawarn <sawarn10@rowan.edu>
2026-07-17 16:57:51 -0400

    Improve formatting of Student and Course instantiation
    Refactor code for better readability by formatting constructor and method calls.

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,45 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Student student = new Student("Siddhi Sawarn", 101);
+        Student student =
+        new Student("Siddhi Sawarn", 101);
 
 
         Course javaCourse =
-        new Course("Object Oriented Programming", 4);
+        new Course(
+            "Object Oriented Programming",
+            4
+        );
 
 
         Course dataCourse =
-        new Course("Data Structures", 3);
+        new Course(
+            "Data Structures",
+            3
+        );
 
 
         student.addCourse(javaCourse);
+
         student.addCourse(dataCourse);
 
 
         System.out.println("Student Information");
 
-        System.out.println("Name: " + student.getName());
+        System.out.println(
+            "Name: " + student.getName()
+        );
 
-        System.out.println("ID: " + student.getId());
+
+        System.out.println(
+            "ID: " + student.getId()
+        );
+
+
+        System.out.println();
+
+
+        student.displayCourses();
 
     }
 }
