aaa88cf66f99ca8c260c4ad1014009333f7f794a
siddhisawarn <sawarn10@rowan.edu>
2026-07-17 17:00:22 -0400

    Improve Student class with GPA calculation method
    Refactor Student class methods and add GPA calculation

--- src/Student.java
+++ src/Student.java
@@ -7,27 +7,27 @@ public class Student extends Person {
     private ArrayList<Course> courses;
 
 
-    public Student(String name, int id){
+    public Student(String name, int id) {
 
-        super(name,id);
+        super(name, id);
 
         courses = new ArrayList<>();
 
     }
 
 
-    public void addCourse(Course course){
+    public void addCourse(Course course) {
 
         courses.add(course);
 
     }
 
 
-    public void displayCourses(){
+    public void displayCourses() {
 
-        System.out.println("Courses:");
+        System.out.println("\nCourses:");
 
-        for(Course course : courses){
+        for(Course course : courses) {
 
             course.displayCourse();
 
@@ -35,5 +35,33 @@ public class Student extends Person {
 
     }
 
+
+    public double calculateGPA() {
+
+        double totalPoints = 0;
+        int totalCredits = 0;
+
+
+        for(Course course : courses) {
+
+            totalPoints += 
+            course.getGradePoint() * course.getCredits();
+
+
+            totalCredits += course.getCredits();
+
+        }
+
+
+        if(totalCredits == 0) {
+
+            return 0;
+
+        }
+
+
+        return totalPoints / totalCredits;
+
+    }
 
 }
