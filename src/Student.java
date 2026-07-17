ad52a7c3ea23de26c735b23e0ba4282cfad61519
siddhisawarn <sawarn10@rowan.edu>
2026-07-17 17:28:00 -0400

    Add course list getter for file saving
    Refactored Student class methods for consistency and readability.

--- src/Student.java
+++ src/Student.java
@@ -7,27 +7,30 @@ public class Student extends Person {
     private ArrayList<Course> courses;
 
 
-    public Student(String name, int id) {
 
-        super(name, id);
+    public Student(String name, int id){
+
+        super(name,id);
 
         courses = new ArrayList<>();
 
     }
 
 
-    public void addCourse(Course course) {
+
+    public void addCourse(Course course){
 
         courses.add(course);
 
     }
 
 
-    public void displayCourses() {
+
+    public void displayCourses(){
 
         System.out.println("\nCourses:");
 
-        for(Course course : courses) {
+        for(Course course : courses){
 
             course.displayCourse();
 
@@ -36,16 +39,21 @@ public class Student extends Person {
     }
 
 
-    public double calculateGPA() {
+
+    public double calculateGPA(){
 
         double totalPoints = 0;
+
         int totalCredits = 0;
 
 
-        for(Course course : courses) {
 
-            totalPoints += 
-            course.getGradePoint() * course.getCredits();
+        for(Course course : courses){
+
+            totalPoints +=
+            course.getGradePoint()
+            * course.getCredits();
+
 
 
             totalCredits += course.getCredits();
@@ -53,15 +61,26 @@ public class Student extends Person {
         }
 
 
-        if(totalCredits == 0) {
+
+        if(totalCredits == 0){
 
             return 0;
 
         }
 
 
+
         return totalPoints / totalCredits;
 
     }
+
+
+
+    public ArrayList<Course> getCourses(){
+
+        return courses;
+
+    }
+
 
 }
